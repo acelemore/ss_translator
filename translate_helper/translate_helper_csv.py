12de64c8fe137bc8c7604e95ab662b3015bfe82f
Lemon Gump <ashlemore@qq.com>
2025-07-29 07:59:26 +0800

    改进OR分割

--- translate_helper/translate_helper_csv.py
+++ translate_helper/translate_helper_csv.py
@@ -255,7 +255,7 @@ def get_text_with_or(text_list: list) -> list:
     
     for i, text in enumerate(text_list):
         # 尝试匹配OR分隔的内容
-        matches = text.split('OR')
+        matches = text.split('\r\nOR\r\n')
         # 为每个匹配的字符串创建TranslationObject
         for match in matches:
             translation_obj = TranslationObject(
